727ba69d933a37652502134ffbfe76f7bf957977
Joseph Mornin <joseph@mornin.org>
2012-05-26 19:15:02 -0400

    Add entry styles

--- index.php
+++ index.php
@@ -43,72 +43,76 @@
             </div><!--/.row-->
         </nav><!--/#main-nav-->
         <section>
-            <article>
+            <article class="entry">
                 <div class="row">
                     <div class="span8">
-                        <h2>Blog post title goes here</h2>
+                        <h2 class="entry-title">Blog post title goes here</h2>
                     </div><!--/.span8-->
                 </div><!--/.row-->
                 <div class="row">
-                    <div class="span2 post-meta">
-                        <p class="post-author">By <a href="javascript:void(0);">Author Name</a></p>
-                        <p class="post-date">January 1, 1970</p>
-                        <p class="post-comments"><a href="javascriptLvoid(0);">23 comments</a></p>
+                    <div class="span2 entry-meta">
+                        <p class="entry-author">By <a href="javascript:void(0);">Author Name</a></p>
+                        <p class="entry-date">January 1, 1970</p>
+                        <p class="entry-comments"><a href="javascriptLvoid(0);">23 comments</a></p>
                         <h3>Tagged:</h3>
-                        <ul>
-                            <li><a href="javascript:void(0);">Tag one</a></li>
-                            <li><a href="javascript:void(0);">Tag two</a></li>
-                            <li><a href="javascript:void(0);">Tag three</a></li>
-                        </ul>
+                        <p>
+                            <a href="javascript:void(0);">Tag one</a>,
+                            <a href="javascript:void(0);">Tag two</a>,
+                            <a href="javascript:void(0);">Tag three</a>,
+                            <a href="javascript:void(0);">Tag four</a>
+                        </p>
                         <h3>Categorized:</h3>
-                        <ul>
-                            <li><a href="javascript:void(0);">Category one</a></li>
-                            <li><a href="javascript:void(0);">Category two</a></li>
-                            <li><a href="javascript:void(0);">Category three</a></li>
-                        </ul>
-                    </div><!--/.span2 post-meta"-->
-                    <div class="span6 post-contents">
+                        <p>
+                            <a href="javascript:void(0);">Category one</a>,
+                            <a href="javascript:void(0);">Category two</a>,
+                            <a href="javascript:void(0);">Category three</a>,
+                            <a href="javascript:void(0);">Category four</a>
+                        </p>
+                    </div><!--/.span2 entry-meta"-->
+                    <div class="span6 entry-contents">
                         <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin blandit dictum eleifend. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum sed tempus odio. Phasellus lobortis suscipit mattis. Nulla facilisi. In nisi turpis, tristique ut accumsan at, sagittis a orci. Ut nisl enim, eleifend quis fermentum commodo, fermentum a velit. Maecenas a augue nec leo fermentum auctor et ut turpis. Donec ultricies ligula sed mi suscipit sagittis.</p>
                         <p>Sed in nisl at est pellentesque posuere pharetra eget nisl. Cras in condimentum ligula. Proin pellentesque, massa eu ultricies lobortis, magna libero auctor lorem, eu adipiscing urna est ut magna. Duis interdum enim auctor sem porta a pretium magna auctor. Duis eget diam a nisi iaculis molestie. Ut porta eros lacus, sit amet elementum arcu.</p>
                         <h3>An interior title could go here</h3>
                         <p>Donec accumsan enim eget libero faucibus auctor. Morbi luctus lorem eget tortor gravida condimentum. Vestibulum quis nulla nec enim porta venenatis in in erat. Maecenas elit diam, sollicitudin sed posuere quis, adipiscing vel nisl. Ut at urna dapibus augue condimentum rutrum.</p>
                         <p>Aliquam erat volutpat. Maecenas ullamcorper volutpat eleifend. Sed vehicula luctus quam ut interdum. Maecenas mi mauris, pharetra sed mattis nec, fringilla quis lectus. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam tellus tortor, scelerisque quis hendrerit tempus, consequat nec lorem. Praesent nec erat et velit interdum suscipit.</p>
-                    </div><!--/.span6 .post-contents-->
+                    </div><!--/.span6 .entry-contents-->
                 </div><!--/.row-->
-            </article>
-            <article>
+            </article><!--/.entry-->
+            <article class="entry">
                 <div class="row">
                     <div class="span8">
-                        <h2>Blog post title goes here</h2>
+                        <h2 class="entry-title">Blog post title goes here</h2>
                     </div><!--/.span8-->
                 </div><!--/.row-->
                 <div class="row">
-                    <div class="span2 post-meta">
-                        <p class="post-author">By <a href="javascript:void(0);">Author Name</a></p>
-                        <p class="post-date">January 1, 1970</p>
-                        <p class="post-comments"><a href="javascriptLvoid(0);">23 comments</a></p>
+                    <div class="span2 entry-meta">
+                        <p class="entry-author">By <a href="javascript:void(0);">Author Name</a></p>
+                        <p class="entry-date">January 1, 1970</p>
+                        <p class="entry-comments"><a href="javascriptLvoid(0);">23 comments</a></p>
                         <h3>Tagged:</h3>
-                        <ul>
-                            <li><a href="javascript:void(0);">Tag one</a></li>
-                            <li><a href="javascript:void(0);">Tag two</a></li>
-                            <li><a href="javascript:void(0);">Tag three</a></li>
-                        </ul>
+                        <p>
+                            <a href="javascript:void(0);">Tag one</a>,
+                            <a href="javascript:void(0);">Tag two</a>,
+                            <a href="javascript:void(0);">Tag three</a>,
+                            <a href="javascript:void(0);">Tag four</a>
+                        </p>
                         <h3>Categorized:</h3>
-                        <ul>
-                            <li><a href="javascript:void(0);">Category one</a></li>
-                            <li><a href="javascript:void(0);">Category two</a></li>
-                            <li><a href="javascript:void(0);">Category three</a></li>
-                        </ul>
-                    </div><!--/.span2 post-meta"-->
-                    <div class="span6 post-contents">
+                        <p>
+                            <a href="javascript:void(0);">Category one</a>,
+                            <a href="javascript:void(0);">Category two</a>,
+                            <a href="javascript:void(0);">Category three</a>,
+                            <a href="javascript:void(0);">Category four</a>
+                        </p>
+                    </div><!--/.span2 entry-meta"-->
+                    <div class="span6 entry-contents">
                         <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin blandit dictum eleifend. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum sed tempus odio. Phasellus lobortis suscipit mattis. Nulla facilisi. In nisi turpis, tristique ut accumsan at, sagittis a orci. Ut nisl enim, eleifend quis fermentum commodo, fermentum a velit. Maecenas a augue nec leo fermentum auctor et ut turpis. Donec ultricies ligula sed mi suscipit sagittis.</p>
                         <p>Sed in nisl at est pellentesque posuere pharetra eget nisl. Cras in condimentum ligula. Proin pellentesque, massa eu ultricies lobortis, magna libero auctor lorem, eu adipiscing urna est ut magna. Duis interdum enim auctor sem porta a pretium magna auctor. Duis eget diam a nisi iaculis molestie. Ut porta eros lacus, sit amet elementum arcu.</p>
                         <h3>An interior title could go here</h3>
                         <p>Donec accumsan enim eget libero faucibus auctor. Morbi luctus lorem eget tortor gravida condimentum. Vestibulum quis nulla nec enim porta venenatis in in erat. Maecenas elit diam, sollicitudin sed posuere quis, adipiscing vel nisl. Ut at urna dapibus augue condimentum rutrum.</p>
                         <p>Aliquam erat volutpat. Maecenas ullamcorper volutpat eleifend. Sed vehicula luctus quam ut interdum. Maecenas mi mauris, pharetra sed mattis nec, fringilla quis lectus. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam tellus tortor, scelerisque quis hendrerit tempus, consequat nec lorem. Praesent nec erat et velit interdum suscipit.</p>
-                    </div><!--/.span6 .post-contents-->
+                    </div><!--/.span6 .entry-contents-->
                 </div><!--/.row-->
-            </article>
+            </article><!--/.entry-->
         </section>
         <hr />
         <footer>
