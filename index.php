0cdcd30d57ab5ac1e4b5504f9a8cb8576278258c
Joseph Mornin <joseph@mornin.org>
2012-05-26 12:51:53 -0400

    Add placeholder blog content

--- index.php
+++ index.php
@@ -9,10 +9,100 @@
     <![endif]--> 
 </head>
 <body>
-    <h1>Hello, world!</h1>
-    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin blandit dictum eleifend. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum sed tempus odio. Phasellus lobortis suscipit mattis. Nulla facilisi. In nisi turpis, tristique ut accumsan at, sagittis a orci. Ut nisl enim, eleifend quis fermentum commodo, fermentum a velit. Maecenas a augue nec leo fermentum auctor et ut turpis. Donec ultricies ligula sed mi suscipit sagittis.</p>
-    <h2>Another heading</h2>
-    <p>Sed in nisl at est pellentesque posuere pharetra eget nisl. Cras in condimentum ligula. Proin pellentesque, massa eu ultricies lobortis, magna libero auctor lorem, eu adipiscing urna est ut magna. Duis interdum enim auctor sem porta a pretium magna auctor. Duis eget diam a nisi iaculis molestie. Ut porta eros lacus, sit amet elementum arcu.</p>
+    <div class="container">
+        <div class="row">
+            <div class="span8">
+                <header>
+                    <h1>Site title</h1>
+                </header>
+            </div><!--/.span8-->
+        </div><!--/.row-->
+        <div class="row">
+            <div class="span8">
+                <nav>
+                    <ul>
+                        <li><a href="javascript:void(0);">Page One</a></li>
+                        <li><a href="javascript:void(0);">Page Two</a></li>
+                        <li><a href="javascript:void(0);">Page Three</a></li>
+                        <li><a href="javascript:void(0);">Page Four</a></li>
+                        <li><a href="javascript:void(0);">Page Five</a></li>
+                    </ul>
+                </nav>
+            </div><!--/.span8-->
+        </div><!--/.row-->
+        <section>
+            <article>
+                <div class="row">
+                    <div class="span8">
+                        <h2>Blog post title goes here</h2>
+                    </div><!--/.span8-->
+                </div><!--/.row-->
+                <div class="row">
+                    <div class="span2 post-meta">
+                        <p class="post-author">By <a href="javascript:void(0);">Author Name</a></p>
+                        <p class="post-date">January 1, 1970</p>
+                        <p class="post-comments"><a href="javascriptLvoid(0);">23 comments</a></p>
+                        <h3>Tagged:</h3>
+                        <ul>
+                            <li><a href="javascript:void(0);">Tag one</a></li>
+                            <li><a href="javascript:void(0);">Tag two</a></li>
+                            <li><a href="javascript:void(0);">Tag three</a></li>
+                        </ul>
+                        <h3>Categorized:</h3>
+                        <ul>
+                            <li><a href="javascript:void(0);">Category one</a></li>
+                            <li><a href="javascript:void(0);">Category two</a></li>
+                            <li><a href="javascript:void(0);">Category three</a></li>
+                        </ul>
+                    </div><!--/.span2 post-meta"-->
+                    <div class="span6 post-contents">
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin blandit dictum eleifend. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum sed tempus odio. Phasellus lobortis suscipit mattis. Nulla facilisi. In nisi turpis, tristique ut accumsan at, sagittis a orci. Ut nisl enim, eleifend quis fermentum commodo, fermentum a velit. Maecenas a augue nec leo fermentum auctor et ut turpis. Donec ultricies ligula sed mi suscipit sagittis.</p>
+                        <p>Sed in nisl at est pellentesque posuere pharetra eget nisl. Cras in condimentum ligula. Proin pellentesque, massa eu ultricies lobortis, magna libero auctor lorem, eu adipiscing urna est ut magna. Duis interdum enim auctor sem porta a pretium magna auctor. Duis eget diam a nisi iaculis molestie. Ut porta eros lacus, sit amet elementum arcu.</p>
+                        <h3>An interior title could go here</h3>
+                        <p>Donec accumsan enim eget libero faucibus auctor. Morbi luctus lorem eget tortor gravida condimentum. Vestibulum quis nulla nec enim porta venenatis in in erat. Maecenas elit diam, sollicitudin sed posuere quis, adipiscing vel nisl. Ut at urna dapibus augue condimentum rutrum.</p>
+                        <p>Aliquam erat volutpat. Maecenas ullamcorper volutpat eleifend. Sed vehicula luctus quam ut interdum. Maecenas mi mauris, pharetra sed mattis nec, fringilla quis lectus. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam tellus tortor, scelerisque quis hendrerit tempus, consequat nec lorem. Praesent nec erat et velit interdum suscipit.</p>
+                    </div><!--/.span6 .post-contents-->
+                </div><!--/.row-->
+            </article>
+            <article>
+                <div class="row">
+                    <div class="span8">
+                        <h2>Blog post title goes here</h2>
+                    </div><!--/.span8-->
+                </div><!--/.row-->
+                <div class="row">
+                    <div class="span2 post-meta">
+                        <p class="post-author">By <a href="javascript:void(0);">Author Name</a></p>
+                        <p class="post-date">January 1, 1970</p>
+                        <p class="post-comments"><a href="javascriptLvoid(0);">23 comments</a></p>
+                        <h3>Tagged:</h3>
+                        <ul>
+                            <li><a href="javascript:void(0);">Tag one</a></li>
+                            <li><a href="javascript:void(0);">Tag two</a></li>
+                            <li><a href="javascript:void(0);">Tag three</a></li>
+                        </ul>
+                        <h3>Categorized:</h3>
+                        <ul>
+                            <li><a href="javascript:void(0);">Category one</a></li>
+                            <li><a href="javascript:void(0);">Category two</a></li>
+                            <li><a href="javascript:void(0);">Category three</a></li>
+                        </ul>
+                    </div><!--/.span2 post-meta"-->
+                    <div class="span6 post-contents">
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin blandit dictum eleifend. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum sed tempus odio. Phasellus lobortis suscipit mattis. Nulla facilisi. In nisi turpis, tristique ut accumsan at, sagittis a orci. Ut nisl enim, eleifend quis fermentum commodo, fermentum a velit. Maecenas a augue nec leo fermentum auctor et ut turpis. Donec ultricies ligula sed mi suscipit sagittis.</p>
+                        <p>Sed in nisl at est pellentesque posuere pharetra eget nisl. Cras in condimentum ligula. Proin pellentesque, massa eu ultricies lobortis, magna libero auctor lorem, eu adipiscing urna est ut magna. Duis interdum enim auctor sem porta a pretium magna auctor. Duis eget diam a nisi iaculis molestie. Ut porta eros lacus, sit amet elementum arcu.</p>
+                        <h3>An interior title could go here</h3>
+                        <p>Donec accumsan enim eget libero faucibus auctor. Morbi luctus lorem eget tortor gravida condimentum. Vestibulum quis nulla nec enim porta venenatis in in erat. Maecenas elit diam, sollicitudin sed posuere quis, adipiscing vel nisl. Ut at urna dapibus augue condimentum rutrum.</p>
+                        <p>Aliquam erat volutpat. Maecenas ullamcorper volutpat eleifend. Sed vehicula luctus quam ut interdum. Maecenas mi mauris, pharetra sed mattis nec, fringilla quis lectus. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam tellus tortor, scelerisque quis hendrerit tempus, consequat nec lorem. Praesent nec erat et velit interdum suscipit.</p>
+                    </div><!--/.span6 .post-contents-->
+                </div><!--/.row-->
+            </article>
+        </section>
+        <hr />
+        <footer>
+            <p>Footer contents</p>
+        </footer>
+    </div><!--/.container-->
 </body>
 </html>
 
