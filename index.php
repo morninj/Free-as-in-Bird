427bd77e97983f23537d696e82342881e2e8ee75
Joseph Mornin <joseph@mornin.org>
2012-05-26 10:50:50 -0400

    Add initial "Hello, world!" template in index.php

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <title></title>
+    <link href="<?php bloginfo('stylesheet_directory'); ?>/style.css" rel="stylesheet" type="text/css" />
+    <!--[if lt IE 9]>  
+    <script src="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script>  
+    <![endif]--> 
+</head>
+<body>
+    <p>Hello, world!</p>
+</body>
+</html>
+
+
