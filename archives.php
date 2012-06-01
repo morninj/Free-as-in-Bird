117f9e1711218d5b34822b39e4ba98ccb497b36f
Joseph Mornin <joseph@mornin.org>
2012-06-01 08:41:23 -0400

    Add blog archives page

---
+++ archives.php
@@ -0,0 +1,23 @@
+<?php
+/*
+Template Name: Archives
+*/
+get_header(); ?>
+
+<?php the_post(); ?>
+<h2 class="entry-title"><?php the_title(); ?></h2>
+
+<?php get_search_form(); ?>
+
+<h2>Archives by Month:</h2>
+<ul>
+        <?php wp_get_archives('type=monthly'); ?>
+</ul>
+
+<h2>Archives by Subject:</h2>
+<ul>
+         <?php wp_list_categories(); ?>
+</ul>
+
+
+<?php get_footer(); ?>
