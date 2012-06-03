dd32d5cb92920590e7927751a3097d7444321bd3
Joseph Mornin <joseph@mornin.org>
2012-06-03 15:25:24 -0400

    Add 500 template

---
+++ 500.php
@@ -0,0 +1,13 @@
+<?php get_header(); ?>
+
+<article id="entry">
+    <div class="row">
+        <div class="span8">
+            <h2 class="entry-title">Server Error</h2>
+            <p>Something went wrong on our end. We're working to fix it.</p>
+        </div><!--/.span8-->
+    </div><!--/.row-->
+</article>
+
+
+<?php get_footer(); ?>
