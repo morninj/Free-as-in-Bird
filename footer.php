5e1632cb0c567398fe80f68099ba3a691da570cc
Joseph Mornin <joseph@mornin.org>
2012-06-03 15:52:47 -0400

    Add footer content

--- footer.php
+++ footer.php
@@ -1,7 +1,32 @@
         </section>
-        <hr />
+        <hr class="footer-divider" />
         <footer>
-            <p>Footer contents</p>
+            <div class="row">
+                <div class="span5" id="col1">
+                    <p>
+                        Lessig
+                        &nbsp;<span>//&nbsp;
+                        <a href="<?php site_url(); ?>/blog/">Blog</a>
+                        &nbsp;&middot;&nbsp;
+                        <a href="<?php site_url(); ?>/books/">Books</a>
+                        &nbsp;&middot;&nbsp;
+                        <a href="<?php site_url(); ?>/topic/news/">News</a>
+                        &nbsp;&middot;&nbsp;
+                        <a href="http://wiki.lessig.org/">Wiki</a>
+                        &nbsp;&middot;&nbsp;
+                        <a href="<?php site_url(); ?>/about/">About</a>
+                    </p>
+                    </p>
+                </div><!--/.span4-->
+                <div class="span3" id="col2">
+                    <a href="<?php bloginfo('rss2_url'); ?>"><img src="<?php bloginfo('stylesheet_directory'); ?>/images/social-media-icons/feed.png" width="16" height="16" alt="Feed" /></a>
+                    <script type="text/javascript">document.write('<a href="mail' + 'to:le' + 'ssig' + '@' + 'pobo' + 'x' + '.c' + 'om">');</script><img src="<?php bloginfo('stylesheet_directory'); ?>/images/social-media-icons/email.png" width="16" height="16" alt="email" /></a>
+                    <a href="https://www.facebook.com/pages/Lawrence-Lessig/105623262805744"><img src="<?php bloginfo('stylesheet_directory'); ?>/images/social-media-icons/facebook.png" width="16" height="16" alt="Facebook" /></a>
+                    <a href="https://twitter.com/lessig"><img src="<?php bloginfo('stylesheet_directory'); ?>/images/social-media-icons/twitter.png" width="16" height="16" alt="Twitter" /></a>
+                    <a rel="license" href="http://creativecommons.org/licenses/by/3.0/"><img alt="Creative Commons License" style="border-width:0" src="http://i.creativecommons.org/l/by/3.0/80x15.png" /></a>
+                </div><!--/.span3-->
+            </div><!--/row-->
+
             <p class="credits">
                 Powered by <a href="http://www.wordpress.org/">WordPress</a> and <a href="https://github.com/morninj/Free-as-in-Bird">Free as in Bird</a>
             </p>
